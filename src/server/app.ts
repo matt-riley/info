51003f489300451baf78e458a8270f721e97da99
Matt Riley <matt@mattriley.me>
2019-01-21 22:53:05 +0000

    Why won't CORS work?

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,19 @@
+import cors from "cors";
 import express from "express";
 import express_enforces_ssl from "express-enforces-ssl";
 import helmet from "helmet";
 
 const app = express();
 app.enable("trust proxy");
+
+const corsOptions = {
+  allowedHeaders: ["Authorization", "Content-Type"],
+  methods: ["POST"],
+  origin: 'https://status.mattriley.info',
+  preflightContinue: false,
+};
+
+app.use(cors(corsOptions));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express_enforces_ssl());
