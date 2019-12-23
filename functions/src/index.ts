142ac61741adcb5baf471bf0d76c21d46922ef64
Matt Riley <matt@mattriley.me>
2019-12-23 13:33:17 +0000

    Reorganised the functions dir

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,45 +1,7 @@
-import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import fetch from 'node-fetch';
-import { DiscogsArtistResponse } from './interfaces/discogsArtistResponse';
-
 admin.initializeApp();
-const firestore = admin.firestore();
 
-export const getFartist = functions.firestore.document('/releases/{releaseId}').onCreate(async (event) => {
-  const data = event.data();
-  if (!data) return false;
+import * as artistInfo from './getArtist';
 
-
-  for (let artist of data.artists) {
-    console.log(artist);
-    await fetch(`https://api.discogs.com/artists/${artist.id}?key=${functions.config().discogs.key}&secret=${functions.config().discogs.secret}`).then(res => {
-      return res.json()
-    }).then(async (json: DiscogsArtistResponse) => {
-      console.log(json);
-      const saveArtistInfo = {
-        description: (json.profile) ? json.profile : '',
-        name: json.name,
-        members: (json.members) ? json.members.map(member => {
-          return {
-            id: member.id
-          }
-        }) : [''],
-        urls: (json.urls) ? json.urls : [''],
-        images: (json.images) ? json.images : [''],
-      }
-      let data;
-      try {
-        data = await firestore.doc(`artists/${artist.id}`).set(saveArtistInfo);
-      }
-      catch (err) {
-        data = console.error(err);
-      }
-      console.info(data);
-    }).catch(err => {
-      console.error(err)
-      throw Error;
-    })
-  }
-  return data;
-})
+export const getArtist = artistInfo.getArtist;
