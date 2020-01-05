f957301f95dc0666d6904d108c78c2277a0bef7f
Matt Riley <matt@mattriley.me>
2020-01-05 15:23:04 +0000

    feat(mc: releases): lists all releases
    re #380

--- src/schema/music/music_collection.ts
+++ src/schema/music/music_collection.ts
@@ -7,15 +7,18 @@ const MusicCollection = objectType({
   description: 'My Music collection',
   name: 'MusicCollection',
   definition(t) {
-    // t.field('releases', {
-    //   description: 'All the releases (i.e. Albums/12"s) in the collection',
-    //   list: [true],
-    //   nullable: false,
-    //   type: Release,
-    //   // async resolve(root, args, context, info) {
-    //   //   return root;
-    //   // },
-    // });
+    t.field('releases', {
+      description: 'All the releases (i.e. Albums/12"s) in the collection',
+      list: [true],
+      nullable: false,
+      type: Release,
+      async resolve(root): Promise<Array<NexusGenFieldTypes['Release']>> {
+        const res = await admin.firestore().collection(`releases`).get();
+        return res.docs.map((doc) => {
+          return doc.data() as NexusGenFieldTypes['Release'];
+        });
+      },
+    });
     t.field('release', {
       args: {
         id: idArg({ description: 'The id of the release' }),
