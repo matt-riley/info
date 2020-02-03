bfcdaf6a8b958c674cb4556257a2880e84583b59
Matt Riley <matt-riley@users.noreply.github.com>
2020-02-03 19:58:42 +0000

    [#66] Added sentry key (#67)

--- api/src/server/app.ts
+++ api/src/server/app.ts
@@ -1,26 +1,31 @@
 import * as Sentry from '@sentry/node';
-import express, { NextFunction } from 'express';
+import express, { NextFunction, Request } from 'express';
 import helmet from 'helmet';
 import '../utils/dotEnv';
 
 const app = express();
-app.enable('trust proxy');
+
+Sentry.init({
+  dsn: process.env.SENTRY_KEY
+})
+
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(Sentry.Handlers.requestHandler());
+
   app.use(helmet.hsts({
     maxAge: 31536000,
     preload: true,
   }));
 
-  app.use(Sentry.Handlers.requestHandler());
   app.use(Sentry.Handlers.errorHandler());
-
-  app.use(function onError(err: any, req: any, res: any, next: NextFunction) {
+  app.use((err: any, req: Request, res: any, next: NextFunction) => {
     res.statusCode = 500;
     res.end(res.sentry + '\n');
   });
 }
 
+app.enable('trust proxy');
 app.disable('x-powered-by');
 
 export default app;
