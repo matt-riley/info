96b8a05966dddcaaa9115d713a1c7b87ac264977
Matt Riley <matt-riley@users.noreply.github.com>
2020-03-08 20:14:29 +0000

    Added bucket (#168)
    * feat(getImages): Cloud Functions to get tmp images (#126)
    
    * Updated getImg function
    
    * Console.log to force deploy

--- cloud-functions/functions/src/updateImgs/index.ts
+++ cloud-functions/functions/src/updateImgs/index.ts
@@ -2,21 +2,25 @@ import * as functions from 'firebase-functions';
 import * as path from 'path';
 // import * as admin from 'firebase-admin';
 
-export const updateImgs = functions.storage.object().onFinalize(async event => {
-  const filePath = event.name;
-  const fileName = path.basename(filePath as string).split('.')[0];
-  const fileNameMatch = fileName.match(/_\d+x\d+$/);
-  if (fileNameMatch && fileNameMatch.length === 0) {
-    console.info('Not a resized image');
-    return false;
-  }
-  // const firestore = admin.firestore();
-  /**
-   * Path is images/<artist|release|label>/<id>/<idx>/<idx>_<width>x<height>.<fileExt>
-   */
-  const link = event.selfLink;
-  const filePathSplit = filePath?.split('/');
-  console.log(filePathSplit);
-  console.log(link);
-  return true;
-});
+export const updateImgs = functions.storage
+  .bucket(functions.config().img.prod)
+  .object()
+  .onFinalize(async event => {
+    const filePath = event.name;
+    const fileName = path.basename(filePath as string).split('.')[0];
+    const fileNameMatch = fileName.match(/_\d+x\d+$/);
+    if (fileNameMatch && fileNameMatch.length === 0) {
+      console.info('Not a resized image');
+      return false;
+    }
+    // const firestore = admin.firestore();
+    /**
+     * Path is images/<artist|release|label>/<id>/<idx>/<idx>_<width>x<height>.<fileExt>
+     */
+    const link = event.selfLink;
+    const filePathSplit = filePath?.split('/');
+    console.log('HERE COME THE GIRLS');
+    console.log(filePathSplit);
+    console.log(link);
+    return true;
+  });
