d4ba6eccf4eba091c8a6801feec026406440c79f
Matt Riley <matt@mattriley.me>
2020-04-05 15:40:26 +0100

    Start of Sapper homepage

---
+++ svelte-home/src/routes/_layout.svelte
@@ -0,0 +1,27 @@
+<script lang="typescript">
+	// @ts-ignore: No default export
+	import Nav from '../components/Nav.svelte'
+
+	export let segment;
+</script>
+
+<style>
+	main {
+		position: relative;
+		display: grid;
+		grid-template-columns: 10vw 2fr;
+		min-height: 100vh;
+		grid-column-gap: 1em;
+	}
+
+	wrapper {
+		padding-top: 1rem;
+	}
+</style>
+
+<main>
+	<Nav {segment}/>
+	<wrapper>
+		<slot></slot>
+	</wrapper>
+</main>
---
+++ svelte-home/src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script lang="typescript">
+	export let title: string = "Hello Typescript"
+</script>
+
+<style>
+	h1 {
+		margin: 0;
+		padding: 0;
+	}
+</style>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
