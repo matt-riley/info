d5b19559cc61c6e0589173d0ae33f269421af05f
Matt Riley <matt@mattriley.me>
2020-04-09 20:07:34 +0100

    Svelte home

--- svelte-home/src/routes/index.svelte
+++ svelte-home/src/routes/index.svelte
@@ -1,16 +1,45 @@
 <script lang="typescript">
-	export let title: string = "Hello Typescript"
+	// @ts-ignore
+	import Card from '../components/Card/index.svelte'
+	let title: string = "Typescript"
+	export const cards = [
+		{
+			title: "Github",
+			img: "https://via.placeholder.com/50"
+		},
+		{
+			title: "Twitter",
+			img: "https://via.placeholder.com/50"
+		},
+		{
+			title: "Music",
+			img: "https://via.placeholder.com/50"
+		},
+		{
+			title: "Insta",
+			img: "https://via.placeholder.com/50"
+		}
+	]
 </script>
 
 <style>
-	h1 {
+	.card-container {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		grid-gap: 1em;
+		width: 100%;
 		margin: 0;
-		padding: 0;
+		height: 50vh;
 	}
+
 </style>
 
 <svelte:head>
 	<title>{title}</title>
 </svelte:head>
 
-<h1>{title}</h1>
+<div class="card-container">
+	{#each cards as card }
+		<Card {...card}/>
+	{/each}
+</div>
