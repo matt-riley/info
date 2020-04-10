a4a32b8c26e803f02a947f35eb3e5cca3164c721
Matt Riley <matt@mattriley.me>
2020-04-10 19:47:38 +0100

    Updated index

--- svelte-home/src/routes/index.svelte
+++ svelte-home/src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script lang="typescript">
 	// @ts-ignore
 	import Card from '../components/Card/index.svelte'
-	let title: string = "Typescript"
+	let title: string = "Matt Riley (dot) Info"
 	export const cards = [
 		{
 			title: "Github",
@@ -23,13 +23,21 @@
 </script>
 
 <style>
+	.wrapper {
+		display: grid;
+		grid-template-rows: repeat(2, 1fr);
+		height: 100%;
+		overflow: hidden;
+	}
 	.card-container {
 		display: grid;
 		grid-template-columns: repeat(4, 1fr);
 		grid-gap: 1em;
 		width: 100%;
 		margin: 0;
-		height: 50vh;
+		height: 35vh;
+		align-self: flex-end;
+		margin-bottom: 1em;
 	}
 
 </style>
@@ -38,8 +46,14 @@
 	<title>{title}</title>
 </svelte:head>
 
-<div class="card-container">
-	{#each cards as card }
-		<Card {...card}/>
-	{/each}
+<div class="wrapper">
+	<header class="header">
+		<h1>PLACEHOLDER</h1>
+		<p>Software Engineer in Leeds, UK.</p>
+	</header>
+	<div class="card-container">
+		{#each cards as card }
+			<Card {...card}/>
+		{/each}
+	</div>
 </div>
