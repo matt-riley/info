696966e25eeb95ba4cdb8dc532492652b88e3805
Matt Riley <matt@mattriley.me>
2020-04-10 18:09:06 +0100

    This isn't right

--- api/src/server/middleware/timeTree.ts
+++ api/src/server/middleware/timeTree.ts
@@ -7,12 +7,12 @@ export const timeTreeMiddleWare = async (resolve: () => any, _parent: any, _args
   try {
     const result = await resolve();
     newTimer.end()
+    timer.end()
+    logger.info(timer.getResult())
     return result
   } catch (error) {
     newTimer.end()
     logger.error(error)
     throw error
   }
-  timer.end()
-  logger.info(timer.getResult())
 }
